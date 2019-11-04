50fb8082bbd1747ae7807c766463373f7e611fa3
user <user@DESKTOP-RP5RJAQ>
2019-11-04 16:42:49 +0600

    task18 done

---
+++ src/assignment3/Task18.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Task18 {
+    
+    public static void main(String[] args){
+    
+     
+        
+    Scanner scanner = new Scanner (System.in);    
+    
+      int number1;
+   
+      System.out.println("Enter the number: ");
+      number1 = scanner.nextInt();
+      
+        for(int i=1; i<=number1; i++){
+       
+        {
+            if(i%2!=0 && i%5!=0)
+            {
+            
+                System.out.println("The numbers are = "+i);   
+    }
+    
+}
+    }
+}
+}
