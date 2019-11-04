4d9dd6fc36329323b13bf5e688a8d1f0303c3465
Shuvo <Shuvo@Shuvo>
2019-11-04 15:59:24 +0600

    Task 12 and 16 done

--- src/assignment3/Task12.java
+++ src/assignment3/Task12.java
@@ -5,6 +5,8 @@
  */
 package assignment3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Shuvo
@@ -15,8 +17,14 @@ public class Task12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //aa
+       
+       Scanner s = new Scanner(System.in);
+       System.out.print("Enter Number : ");
+       int a = s.nextInt();
+       
+       int result = a*(a+1)/2;
+        System.out.println("Sum of these number is "+result);
+        
     }
     
 }
