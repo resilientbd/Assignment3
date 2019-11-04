33ed20b00c5e3560723b046b64f5cfb5144beeec
SAIFUL-ISLAM1 <57309173+SAIFUL-ISLAM1@users.noreply.github.com>
2019-11-04 16:23:02 +0600

    task 11 done task 14 done task 15 done task 16 done task 17 done task 18 done task 19 done task 12 done task 13 done

---
+++ src/assignment3/Task20.java
@@ -0,0 +1,23 @@
+
+package assignment3;
+
+import java.util.Scanner;
+
+
+public class Task20 {
+    
+    public static void main(String[] args) {
+        Scanner MyScan = new Scanner(System.in);
+        
+        System.out.println("Enter your number = ");
+        
+        int num = MyScan.nextInt();
+        if(num>=50){
+            System.out.println("Pass");
+        }
+        else{System.out.println("Fail");
+        
+        }
+    }
+    
+}
