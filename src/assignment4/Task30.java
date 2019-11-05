24a345eb990b6dc7068ec9b1e5ae818d5e093a6a
user <user@DESKTOP-RP5RJAQ>
2019-11-05 15:41:53 +0600

    task30 done

---
+++ src/assignment4/Task30.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Task30 {
+    
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int number, result;
+        
+        System.out.print("Enter the number of published articles for one writer = ");
+        number = scanner.nextInt();
+        
+        result = number * 500;
+        System.out.println(" Monthly fees is : "+ result);
+        
+    }
+    
+}
