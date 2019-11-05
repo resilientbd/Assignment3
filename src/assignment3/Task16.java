42cd233444afdf4752a6b6946e1a5e7d8d5345f7
USER <USER@DESKTOP-CRCSDQP>
2019-11-05 16:32:27 +0600

    All is done.

---
+++ src/assignment3/Task16.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3;
+
+import java.util.Scanner;
+
+
+public class Task16 {
+     public static void main(String[] args) {
+      System.out. println("Enter the Number : ");
+        Scanner in = new Scanner(System.in);
+        int a = in. nextInt();
+        
+       if(a%2==0 && a%5==0){
+                          System.out. println("it is a multiple of either 2 or 5 ");
+                          
+                     } 
+        else
+              {
+                System.out. println(" it is a multiple of either 2 or 5 but not both ");
+                }
+     }
+     
+     }
+
